726cd3a717eca7c4a538488542be36c34ce215bf
Gil Junior <juniorsouza1001@gmail.com>
2026-04-13 05:40:34 +0000

    Adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home () {
+  return (
+    <div>
+      <h1>Meu chamego eu te amo muito! Se você me ama, dê uma risadinha e balançe a raba vai</h1>
+      <img
+        src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnlib2M4aTU1eTA2NmEwc3B2cnZiYm0xd3k5bzV2cXRxNTFtMzlqcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/oQAZtYjeFq3cQ0Gyzq/giphy.gif"
+        alt="Mulher dançando nno estúdio"
+        style={{ width: '300px', borderRadius: '12px' }}
+      />
+    </div>
+  );
+}
+
+export  default Home;

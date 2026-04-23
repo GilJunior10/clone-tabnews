9414f700da96cf64afb10a25500c0c90150c5b03
Gil Junior <juniorsouza1001@gmail.com>
2026-04-23 05:57:23 +0000

    adiciona arquivo `.prettierignore` e `.pages/indeex.js` atualizado para mostrar mensagem de construção

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,36 @@
 function Home() {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        textAlign: "center",
+        padding: "20px",
+      }}
+    >
       <h1>
-        Meu chamego eu te amo muito! Se você me ama, dê uma risadinha e balançe
-        a raba vai
+        Em Processo de construção...{" "}
+        <span role="img" aria-label="construção">
+          🚧
+        </span>
       </h1>
       <img
-        src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnlib2M4aTU1eTA2NmEwc3B2cnZiYm0xd3k5bzV2cXRxNTFtMzlqcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/oQAZtYjeFq3cQ0Gyzq/giphy.gif"
-        alt="Mulher dançando nno estúdio"
-        style={{ width: "300px", borderRadius: "12px" }}
+        src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjhrMGxnemkxZ253eTN5aTVjdHI5aWVyam8wNXVjd3Aza2R1b2N4ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qWx5C3iSfwa6Q/giphy.gif"
+        alt="Construindo o nosso site"
+        style={{ width: "300px", borderRadius: "12px", marginTop: "20px" }}
       />
+
+      <audio autoPlay loop>
+        <source
+          src="https://www.mariomayhem.com/downloads/super_mario_world_music/super_mario_world_overworld_theme.mp3"
+          type="audio/mpeg"
+        />
+      </audio>
     </div>
   );
 }
 
-function test2() {
-  console.log("indentação errada");
-}
 export default Home;

080fb3622593fb7f080254ee9fc7b099bce0b5e7
Gil Junior <juniorsouza1001@gmail.com>
2026-07-11 08:03:04 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(request, response) {
-  response.status(200).json({ chave: "É o melhor dev que vc já conheceu" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response
+    .status(200)
+    .json({ chave: "É o melhor dev que vc já conheceu e com novas versões" });
 }
 
 export default status;
